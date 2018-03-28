a600fea264e0e2f491bcbbcda842fea99411f5be
Julian P Samaroo <jpsamaroo@gmail.com>
2018-03-28 05:57:58 -0500

    Removed layout, groups, and threading from Agent, as they don't need to be inside the agent itself. Disabled evolution-related tests.

--- src/agent.jl
+++ src/agent.jl
@@ -1,40 +1,18 @@
 export Agent
 export sync!, addnode!, delnode!, addedge!, deledge!, merge, merge!, barrier
-export clone, mutate!
 export activate!, deactivate!
 export relocate!, store!, sync!, addnode!, delnode!, addedge!, deledge!, merge, merge!, barrier
-export mutate!
 export run_edges!, run_nodes!, run!
-
-mutable struct AgentRunTable
-  worklists::Dict{Int, Vector{String}}
-  locklist::Dict{String, Bool}
-  lock::Threads.TatasLock
-end
-AgentRunTable() = AgentRunTable(Dict{Int,Vector{String}}([(idx, String[]) for idx in 1:Threads.nthreads()]), Dict{String,Bool}(), Threads.TatasLock())
 
 @nodegen mutable struct Agent
   nodes::Dict{String, AbstractContainer}
   edges::Vector{Tuple{String, String, Symbol}}
-  layout::Dict{String, Union{Dict, String}}
-  groups::Dict{String, Vector{String}}
-  rt::AgentRunTable
 end
-Agent() = Agent(Dict{String,AbstractContainer}(), Tuple{String,String,Symbol}[], Dict{String,Union{Dict,String}}(), Dict{String,Vector{String}}(), AgentRunTable())
+Agent() = Agent(Dict{String,AbstractContainer}(), Tuple{String,String,Symbol}[])
 
-# Retrieve layout item
-getindex(agent::Agent, idx::String) = agent.layout[idx]
-
-# Set layout item (shorthand for merge!())
-setindex!(agent1::Agent, agent2::Agent, idx::String) = merge!(agent1, agent2; name=idx)
-
-# Adds a node, with the option of a top-level name
-function addnode!(agent::Agent, node::AbstractNode; name::String="")
+# Adds a node
+function addnode!(agent::Agent, node::AbstractNode)
   id = randstring(16)
-  if name != ""
-    @assert !haskey(agent.layout, name) "Name $name already exists in layout"
-    agent.layout[name] = id
-  end
   agent.nodes[id] = CPUContainer(node)
   return id
 end
@@ -70,15 +48,10 @@ function merge(agent1::Agent, agent2::Agent)
   return agent3
 end
 
-# Merges agent2 into agent1, with the option of making agent2 a sub-layout of agent1
-function merge!(agent1::Agent, agent2::Agent; name::String="")
+# Merges agent2 into agent1
+function merge!(agent1::Agent, agent2::Agent)
   Base.merge!(agent1.nodes, agent2.nodes)
   agent1.edges = vcat(agent1.edges, agent2.edges)
-  if name != ""
-    agent1.layout[name] = agent2.layout
-  else
-    Base.merge!(agent1.layout, agent2.layout)
-  end
   return agent1
 end
 
@@ -88,46 +61,6 @@ function barrier(agent::Agent)
     barrier(node)
   end
 end
-
-# Clones an agent in its entirety
-function clone(agent::Agent)
-  cloneAgent = Agent()
-  for objIdx in keys(agent.nodes)
-    cloneAgent.nodes[objIdx] = clone(agent.nodes[objIdx])
-  end
-  cloneAgent.edges = copy(agent.edges)
-  return cloneAgent
-end
-
-# Activates a group
-# TODO: madvise?
-function activate!(agent::Agent, group::String)
-  for idx in agent.groups[group]
-    cont = agent.nodes[idx]
-    if cont isa InactiveContainer
-      agent.nodes[idx] = CPUContainer(transient(cont))
-    end
-  end
-end
-
-# Deactivates a group
-# FIXME: Sync transient to root before deactivating
-# TODO: madvise?
-function deactivate!(agent::Agent, group::String)
-  for idx in agent.groups[group]
-    cont = agent.nodes[idx]
-    if !(cont isa InactiveContainer)
-      agent.nodes[idx] = InactiveContainer(transient(cont))
-    end
-  end
-end
-
-# Mutates an agent with probability p for each mutatable parameter
-#=function mutate!(agent::Agent, p::Real)
-  for node in values(agent.nodes)
-    mutate!(node, p)
-  end
-end=#
 
 # Clears any transient data in an agent (such as learned weights or temporary values)
 function clear!(agent::Agent)
@@ -142,7 +75,6 @@ function Base.show(io::IO, agent::Agent)
   println(io, "Agent:")
   println(io, "  $(length(agent.nodes)) nodes")
   println(io, "  $(length(agent.edges)) edges")
-  println(io, "  $(length(agent.groups)) groups")
 end
 
 # Prints a long representation of the agent
@@ -150,24 +82,13 @@ end
 # TODO: Optionally elaborate nodes, edges, groups?
 function Base.showall(io::IO, agent::Agent)
   Base.show(io, agent)
-  println(io, "  Layout:")
-  for (name,value) in agent.layout
-    # TODO: Recursively descend into value if possible
-    println(io, "    $name: $value")
-  end
 end
 
 # Runs all nodes for one iteration
 function run_nodes!(agent::Agent)
   nodes = collect(values(agent.nodes))
-  if canthread()
-    Threads.@threads for node in nodes
-      nupdate!(node)
-    end
-  else
-    for node in nodes
-      nupdate!(node)
-    end
+  for node in nodes
+    nupdate!(node)
   end
 end
 
@@ -185,65 +106,10 @@ function run_edges!(agent::Agent)
       edges[name] = Tuple{Symbol,AbstractContainer}[entry]
     end
   end
-
-  # Mark all edges as ready-to-run
-  # Current algorithm: Round-robin
-  # TODO: Use more cache-friendly algorithm
-  tidx = 1
-  maxTidx = Threads.nthreads()
-  for edgeName in keys(edges)
-    push!(agent.rt.worklists[tidx], edgeName)
-    tidx = (tidx >= maxTidx ? 1 : tidx + 1)
-  end
   
   # Run edges
-  if canthread()
-    Threads.@threads for tidx in 1:Threads.nthreads()
-      # Wait for lock
-      lock(agent.rt.lock)
-
-      # Check all edgesets
-      completed = nothing
-      for edgeIdx in agent.rt.worklists[tidx]
-        # Check if full edgeset is available
-        nodes = map(edgeTuple->edgeTuple[2], filter(edgeTuple->edgeTuple[1]==edgeIdx, agent.edges))
-        if (!haskey(agent.rt.locklist,edgeIdx)||agent.rt.locklist[edgeIdx] == false) &&
-          all(node->(!haskey(agent.rt.locklist,node)||agent.rt.locklist[node]==false), nodes)
-          # Add edgeset to locklist
-          agent.rt.locklist[edgeIdx] = true
-          for node in nodes
-            agent.rt.locklist[node] = true
-          end
-          unlock(agent.rt.lock)
-
-          # Run edgeset
-          node = agent.nodes[edgeIdx]
-          edgeset = Tuple{Symbol,AbstractContainer}[edges[edgeIdx]...]
-          eforward!(node, edgeset)
-
-          # Mark edgeset as done
-          completed = edgeIdx
-
-          # Clear edgeset from locklist
-          lock(agent.rt.lock)
-          agent.rt.locklist[edgeIdx] = false
-          for node in nodes
-            agent.rt.locklist[node] = false
-          end
-        end
-      end
-      if completed != nothing
-        idx = findfirst(edgeIdx->edgeIdx==completed, agent.rt.worklists[tidx])
-        deleteat!(agent.rt.worklists[tidx], idx)
-      end
-
-      # Pass to next thread and wait
-      unlock(agent.rt.lock)
-    end
-  else
-    for edgeName in keys(edges)
-      eforward!(agent.nodes[edgeName], edges[edgeName])
-    end
+  for edgeName in keys(edges)
+    eforward!(agent.nodes[edgeName], edges[edgeName])
   end
 end
 
--- test/core/core-test.jl
+++ test/core/core-test.jl
@@ -1,44 +1,37 @@
 @testset "Core Test" begin
   agent = Agent()
-  n1 = GenericNeurons(8)
-  addnode!(agent, n1; name="N1")
+  n1 = addnode!(agent, GenericNeurons(8))
   @test length(agent.nodes) == 1
-  n2 = GenericNeurons(8)
-  addnode!(agent, n2; name="N2")
-  s1 = GenericSynapses(8, 8)
-  addnode!(agent, s1; name="S1")
-  addedge!(agent, agent["S1"], (
-    (agent["N1"], :input),
-    (agent["N2"], :output)
+  n2 = addnode!(agent, GenericNeurons(8))
+  s1 = addnode!(agent, GenericSynapses(8, 8))
+  addedge!(agent, s1, (
+    (n1, :input),
+    (n2, :output)
   ))
   @test length(agent.edges) == 2
   run!(agent)
 
-  nt = GenericNeurons(8)
-  addnode!(agent, nt; name="NT")
-  st = GenericSynapses(8, 8)
-  addnode!(agent, st; name="ST")
-  addedge!(agent, agent["ST"], (
-    (agent["N1"], :input),
-    (agent["NT"], :output)
+  nt = addnode!(agent, GenericNeurons(8))
+  st = addnode!(agent, GenericSynapses(8, 8))
+  addedge!(agent, st, (
+    (n1, :input),
+    (nt, :output)
   ))
-  deledge!(agent, agent["ST"], agent["N1"], :input)
+  deledge!(agent, st, n1, :input)
   @test length(agent.edges) == 3
-  deledge!(agent, agent["ST"], agent["NT"], :output)
-  delnode!(agent, agent["ST"])
+  deledge!(agent, st, nt, :output)
+  delnode!(agent, st)
   @test length(agent.nodes) == 4
-  delnode!(agent, agent["NT"])
+  delnode!(agent, nt)
 
   # Merge
   agent2 = Agent()
-  n3 = GenericNeurons(16)
-  addnode!(agent2, n3; name="N3")
+  n3 = addnode!(agent2, GenericNeurons(16))
   merge!(agent, agent2)
-  s2 = GenericSynapses(8, 16)
-  addnode!(agent, s2; name="S2")
-  addedge!(agent, agent["S2"], (
-    (agent["N1"], :input),
-    (agent["N3"], :output)
+  s2 = addnode!(agent, GenericSynapses(8, 16))
+  addedge!(agent, s2, (
+    (n1, :input),
+    (n3, :output)
   ))
   run!(agent)
   
